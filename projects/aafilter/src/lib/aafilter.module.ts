0ddf3fb9c8e40a7c53f97f0706601f74e09f4ccc
marcoshannecke <mhannecke@align-alytics.com>
2020-05-29 21:52:44 +0100

    angular 9

--- projects/aafilter/src/lib/aafilter.module.ts
+++ projects/aafilter/src/lib/aafilter.module.ts
@@ -2,20 +2,18 @@ import {NgModule} from '@angular/core';
 import {AafilterComponent, DisplayFormatPipe, HideDimmedPipe, IsDatePipe} from './aafilter.component';
 import {FormsModule} from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
-import {
-  MatButtonModule,
-  MatCardModule,
-  MatCheckboxModule,
-  MatChipsModule,
-  MatDialogModule,
-  MatGridListModule,
-  MatIconModule,
-  MatInputModule,
-  MatListModule,
-  MatProgressSpinnerModule
-} from '@angular/material';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatChipsModule} from '@angular/material/chips';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import {MatListModule} from '@angular/material/list';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 import {CommonModule} from '@angular/common';
 import {ScrollingModule} from '@angular/cdk/scrolling';
+import {MatGridListModule} from '@angular/material/grid-list';
 
 
 @NgModule({
